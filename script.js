e537a54653b6f2d043594c39d13317786dc4392c
vinay021194 <vinaysaxena094@gmail.com>
2024-04-12 19:58:48 +0530

    my first commet

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function appendToDisplay(value) {
 function clearDisplay() {
   document.getElementById("display").value = "";
 }
-
+function backspace() {
+  let currentValue = document.getElementById("display").value;
+  document.getElementById("display").value = currentValue.slice(0, -1);
+}
 function calculate() {
   let expression = document.getElementById("display").value;
   let result = eval(expression);
